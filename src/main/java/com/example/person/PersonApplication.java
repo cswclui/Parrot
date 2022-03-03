8e828b150f05176270f53c900a19a20cde694be0
Richard Lui <test@test.com>
2022-03-03 05:14:58 +0000

    update

---
+++ src/main/java/com/example/person/PersonApplication.java
@@ -0,0 +1,13 @@
+package com.example.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApplication {
+	public static void main(String[] args) {
+		var context = SpringApplication.run(PersonApplication.class, args);
+		Person p = context.getBean(Person.class);
+		System.out.println(p.getParrot().getName());
+	}
+}
