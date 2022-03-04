24735f940cc849b2b42a6d2fd75e5c645e245a7b
cswclui <rwclui@hotmail.com>
2022-03-04 15:04:33 +0800

    Update PersonApplication.java

--- src/main/java/com/example/person/PersonApplication.java
+++ src/main/java/com/example/person/PersonApplication.java
@@ -8,6 +8,7 @@ public class PersonApplication {
 	public static void main(String[] args) {
 		var context = SpringApplication.run(PersonApplication.class, args);
 		Person p = context.getBean(Person.class);
+		System.out.println(p.getName());
 		System.out.println(p.getParrot().getName());
 	}
 }
